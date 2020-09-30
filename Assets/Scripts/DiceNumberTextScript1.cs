a9bf0f6f6c1da1c0612e24d99af96405ac501171
Paul S <paul@g.de>
2020-09-30 22:14:22 +0200

    initial start

---
+++ Assets/Scripts/DiceNumberTextScript1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceNumberTextScript1 : MonoBehaviour {
+
+	Text text;
+	public static int diceNumber1;
+	public static int diceNumber2;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			if (text.name == "Text")
+		{
+			text.text = diceNumber1.ToString();
+		}
+		if (text.name == "Text2")
+		{
+			text.text = diceNumber2.ToString();
+		}
+	}
+}
---
+++ Assets/Scripts/DiceScript1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceScript1 : MonoBehaviour {
+
+	static Rigidbody rb;
+	public static Vector3 diceVelocity;
+
+
+
+
+
+	// Use this for initialization
+	public void Start () {
+
+
+			rb = GetComponent<Rigidbody> ();
+	
+
+			DiceNumberTextScript1.diceNumber1 = 0;
+		DiceNumberTextScript1.diceNumber2 = 0;
+		float dirX = Random.Range (0, 500);
+			float dirY = Random.Range (0, 500);
+			float dirZ = Random.Range (0, 500);
+			transform.position = new Vector3 (Random.Range(0,5), Random.Range(0, 5), Random.Range(0, 5));
+			transform.rotation = Quaternion.identity;
+			rb.AddForce (transform.up * 500);
+			rb.AddTorque (dirX, dirY, dirZ);
+		
+	}
+}
